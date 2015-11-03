0b5fcb9ee9f76b8742eeaf0960c8b6871edecf51
Egor <egor.kozitski@mail.citytech.cuny.edu>
2015-11-03 17:50:39 -0500

    Partial Implementation of WheelConfig

--- WheelConfig.java
+++ WheelConfig.java
@@ -4,11 +4,15 @@
 * @author Egor Kozitski
 * */
 public class WheelConfig  extends java.lang.Object implements Configuration{
+  //@param BRIDGE_STR
    static java.lang.String BRIDGE_STR;
+    //@param EMPTY_CELL
    static int EMPTY_CELL;
+    //@param TRIAD_CELL
    static int TRIAD_CELL;
+    //@param TRIAD_STR
    static java.lang.String TRIAD_STR;
-    WheelConfig(java.lang.String filename){
+    WheelConfig(java.lang.String filename)throws java.io.FileNotFoundException{
 
     }
 }
