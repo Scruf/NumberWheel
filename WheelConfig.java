97248b69a5816a9f8c1af88287d7d5557e89bceb
scruf <ek5442@cs.rit.edu>
2015-11-04 15:15:51 -0800

    Copy Constructor Implemented

--- WheelConfig.java
+++ WheelConfig.java
@@ -9,28 +9,39 @@ import java.util.*;
 * */
 public class WheelConfig  extends java.lang.Object implements Configuration{
   //@param BRIDGE_STR
-   static final java.lang.String BRIDGE_STR="-";
+   public static final java.lang.String BRIDGE_STR="-";
     //@param EMPTY_CELL
-   static int EMPTY_CELL=0;
+   public static int EMPTY_CELL=0;
     //@param TRIAD_CELL
-   static int TRIAD_CELL=3;
-    //@param TRIAD_STR
-   static java.lang.String TRIAD_STR=".";
-    //Constructs an initial number wheel puzzle from an input file whose format is: #_triads bridge1_value bridge2_value...A
+   public static int TRIAD_CELL=3;
+    //@ri TRIAD_STR
+   public static java.lang.String TRIAD_STR=".";
+   public ArrayList<Integer> list;
+   private int []configArray;
+    //Constructs an ri number wheel puzzle from an input file whose format is: #_triads bridge1_value bridge2_value...A
    public WheelConfig(java.lang.String filename)throws java.io.FileNotFoundException{
         String content=" ";
+        ArrayList<String> contentList = new ArrayList<String>();
        try{
             BufferedReader  reader = new BufferedReader(new FileReader(filename));
            while((content = reader.readLine())!=null){
-
+             contentList.add(content);
            }
+           reader.close();
         }catch(java.io.IOException file){
-           System.out.print(file);
+
        }
+       list = new ArrayList<Integer>();
+       list.add(Integer.parseInt(contentList.get(0)));
+       for(String s : contentList.get(1).split(" "))
+        list.add(Integer.parseInt(s));
+      configArray = new int[TRIAD_CELL*list.get(0)];
+      Arrays.fill(configArray,EMPTY_CELL);
     }
     //Copy constructor. Takes an incoming config and makes a complete deep copy of its attributes.
     public WheelConfig(WheelConfig config){
 
+        this.configArray = config.configArray.clone();
     }
     //Get the collection of successors from the current one
     public java.util.Collection<Configuration> getSuccessors(){
@@ -46,6 +57,14 @@ public class WheelConfig  extends java.lang.Object implements Configuration{
     }
     @Override
     public java.lang.String toString(){
-        return " ";
+      String temp =" ";
+      for(int i=0;i<configArray.length;i++){
+        if(i%3==0)
+          temp+=BRIDGE_STR;
+        temp += String.valueOf(configArray[i])+TRIAD_STR;
+
+      }
+      String str = temp.substring(2,temp.length()-1);
+      return str;
     }
 }
